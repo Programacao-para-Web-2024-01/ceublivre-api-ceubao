002a9e360732fcbc9b318d3618d299b91fd6e812
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2024-06-10 07:52:49 -0300

    Delete Codigo_atualizado directory

--- Codigo_atualizado/produto/model.go
+++
@@ -1,9 +0,0 @@
-package produto
-
-type Produto struct {
-	Id        int     `json:"id"`
-	Name      string  `json:"name"`
-	Descricao string  `json:"descricao"`
-	Preco     float64 `json:"preco"`
-	Categoria string  `json:"categoria"`
-}
