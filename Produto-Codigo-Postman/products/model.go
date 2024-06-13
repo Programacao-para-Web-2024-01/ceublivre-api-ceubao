8f3634c6ff87f3082692c756af40ee13c6a0ef4e
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2024-06-12 21:09:34 -0300

    Delete Produto-Codigo-Postman directory

--- Produto-Codigo-Postman/products/model.go
+++
@@ -1,9 +0,0 @@
-package products
-
-type Product struct {
-	Id                    int64   `json:"id"`
-	Name                  string  `json:"name"`
-	Price                 float64 `json:"price"`
-	Description           string  `json:"description"`
-	Categoria_idcategoria int64   `json:"categoria_idcategoria"`
-}
