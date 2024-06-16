68a43430a95a0f55709cde1f586b4bd3674b8f64
Diogo Borges de Moura <111078132+DigogSXD@users.noreply.github.com>
2024-06-16 14:34:44 -0300

    Delete ceublivre-api-ceubao-main/Produto-Codigo-Postman directory

--- ceublivre-api-ceubao-main/Produto-Codigo-Postman/products/model.go
+++
@@ -1,9 +0,0 @@
-package products
-
-type Product struct {
-	ID                    int     `json:"id"`
-	Name                  string  `json:"name"`
-	Description           string  `json:"description"`
-	Price                 float64 `json:"price"`
-	Categoria_idcategoria int     `json:"categoria_idcategoria"`
-}
